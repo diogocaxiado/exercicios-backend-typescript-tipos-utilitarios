3620e2e39fa139420044dc224ca2ad99b50cb59f
Diogo Caxiado <diogocaxiado2004@gmail.com>
2024-01-09 19:47:39 -0300

    Resolvido 02

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,38 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> & {endereco: Endereco, tipoTransacao: Lowercase<'credito' | 'Debito'>}
+
+const venda: NovoCarrinho = {
+    item: {
+        nome: 'Caneca',
+        descricao: 'string',
+        valor: 123
+    },
+    qtd: 2,
+    desconto: 0,
+    frete: 10,
+    tipoTransacao: 'debito',
+    cartao: {
+        numero: 123,
+        validade: '10/12',
+        nome: 'Diogo Caxiado',
+        cvv: 493
+    },
+    endereco: {
+        cep: '2193145',
+        rua: 'Rua Carvalho de Almeida',
+        bairro: 'string',
+        cidade: 'string',
+        estado: 'string'
+    }
+}
